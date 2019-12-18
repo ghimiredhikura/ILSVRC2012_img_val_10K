e053308469133f0457c6e5946169bf439cd63691
Deepak Ghimire <ghmdeepak@gmail.com>
2019-12-18 19:00:36 +0900

    Update nvisoeval.cpp

--- nvisoeval/nvisoeval.cpp
+++ nvisoeval/nvisoeval.cpp
@@ -137,7 +137,7 @@ int main(int argc, char* argv[])
         
         cout << "Image: " << values[0] << endl;
 
-        int image_id = atoi(findAndReplaceAll(values[0], "ILSVRC2012_val_", ""));
+        int image_id = atoi(findAndReplaceAll(values[0], "ILSVRC2012_val_", "").c_str());
         
         stringstream linestream2(values[1]);
         std::vector<pred> m_pred;
