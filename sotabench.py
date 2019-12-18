2af7105a2bcf010e578e7ec150fb72f7be98a863
Deepak Ghimire <ghmdeepak@gmail.com>
2019-12-18 16:18:39 +0900

    Update sotabench.py

--- sotabench.py
+++ sotabench.py
@@ -6,14 +6,18 @@ evaluater = ImageNetEvaluator(
             model_name='MobileNet-v2',
             paper_arxiv_id='1801.04381')
 
+image_ids = []
+preds = []
 with open('mobilenet_v2_imagenet.txt', 'r') as file:
     lines = file.readlines()
     for line in lines:
-        image_id, preds = line.split(":")
+        image_id, pred = line.split(":")
         image_id = image_id.rstrip().lstrip()
-        preds = preds.lstrip().rstrip()
-        preds = np.fromstring(preds, dtype=float, sep=' ')
-        
-        evaluater.add({image_id : preds})
+        pred = pred.lstrip().rstrip()
+        pred = np.fromstring(pred, dtype=float, sep=' ')
+        image_ids.append(image_id)
+        preds.append(pred)
+
+    evaluater.add(dict(zip(image_ids, preds)))
 
     evaluater.get_results()
